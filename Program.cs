8419c89adadf141dd6218590739408d9ebfa74fe
EstherVaz <69050860+EstherVaz@users.noreply.github.com>
2020-10-02 16:27:08 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TheStore.BL.EntidadesNegocio;
+using TheStore.BL.Repository;
+
+
+namespace TheStore.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listRepository =
+                new CustomerRepositoryList();
+            var newCustomer = new SilverCustomer();
+            var adressRepository = new AdressRepository();
+
+
+            newCustomer.Id = 5;
+            newCustomer.Name = "Gilberto";
+            newCustomer.Email = "gil34@gmail.com";
+            newCustomer.Adresses = adressRepository.GetByAdressesById(5);
+
+            listRepository.Save(newCustomer);
+            var customers = listRepository.GetAll();
+
+
+            foreach(var customer in customers)
+            {
+                System.Console.WriteLine(customer.Name);
+            }
+        }
+    }
+}
